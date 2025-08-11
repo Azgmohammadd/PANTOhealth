def2518ffbcae16bfd7612e8e2e5a34106db25d9
mohammad.azghandi <mohammad.azghandi@partsoftware.com>
2025-08-11 15:52:07 +0330

    :sparkles: feat: add logerService

--- phanto-gateway/src/app.module.ts
+++ phanto-gateway/src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TrackProcessorModule } from './modules/trackProcessor/trackProcessor.module';
+import { LoggerService } from './infrastructure/logger/logger.service';
 
 @Module({
   imports: [TrackProcessorModule],
--- phanto-gateway/src/main.ts
+++ phanto-gateway/src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { setupSwagger } from './infrastructure/swagger/swagger.setup';
+import { LoggerService } from './infrastructure/logger/logger.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+  });
+
+  const loggerService = app.get(LoggerService);
+  app.useLogger(loggerService);
+  app.use(loggerService.httpMiddleware);
 
   setupSwagger(app);
   await app.listen(process.env.PORT ?? 3000);
