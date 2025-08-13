02c00497b1d5724e7f8871f4dc84e2b6590ca279
azgmohammadd <azgmohammadd@gmail.com>
2025-08-13 21:39:27 +0330

    :goal_net: feat: add error handling

---
+++ phanto-gateway/src/common/filters/global-exception.filter.ts
@@ -0,0 +1,34 @@
+/* eslint-disable */
+
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+
+@Catch()
+export class GlobalExceptionFilter implements ExceptionFilter {
+  catch(exception: unknown, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse();
+
+    let status = HttpStatus.INTERNAL_SERVER_ERROR;
+    let errorResponse = {
+      code: 'UNKNOWN_ERROR',
+      message: 'An unexpected error occurred.',
+    };
+
+    if (exception instanceof HttpException) {
+      const res: any = exception.getResponse();
+      status = exception.getStatus();
+      errorResponse = {
+        code: res.code || 'UNKNOWN_ERROR',
+        message: res.message || 'An error occurred.',
+      };
+    }
+
+    response.status(status).json(errorResponse);
+  }
+}
