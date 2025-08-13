795cfc6052719d91d472fb17a4fbb597b24f83e7
azgmohammadd <azgmohammadd@gmail.com>
2025-08-13 21:44:35 +0330

    :sparkles: feat: add pipes, filters, and middlewares

--- phanto-gateway/src/main.ts
+++ phanto-gateway/src/main.ts
@@ -1,20 +1,33 @@
+import { ValidationPipe, VersioningType } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
+import { GlobalExceptionFilter } from './common/filters/global-exception.filter';
+import { LoggingInterceptor } from './common/interceptors/logger.interceptor';
 import { setupSwagger } from './infrastructure/swagger/swagger.setup';
 import { LoggerService } from './modules/logger/logger.service';
-import { ValidationPipe } from '@nestjs/common';
-import { LoggingInterceptor } from './common/interceptors/logger.interceptor';
 
 async function bootstrap() {
   const logger = new LoggerService();
   const app = await NestFactory.create(AppModule, {
     logger,
+    cors: true,
   });
+
+  const configService = app.get(ConfigService);
 
   app.useGlobalInterceptors(new LoggingInterceptor(logger));
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new GlobalExceptionFilter());
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  });
+  app.use(helmet());
 
   setupSwagger(app);
-  await app.listen(process.env.PORT ?? 3000);
+
+  await app.listen(configService.get<number>('PORT', 3000));
 }
 bootstrap();
