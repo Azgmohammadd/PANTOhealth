e8a711860dfd70357d659905d5c1706321e27cf6
azgmohammadd <azgmohammadd@gmail.com>
2025-08-13 21:38:47 +0330

    :fire: refactor: remove comment

--- phanto-gateway/src/infrastructure/swagger/swagger.setup.ts
+++ phanto-gateway/src/infrastructure/swagger/swagger.setup.ts
@@ -1,12 +1,11 @@
 import { INestApplication } from '@nestjs/common';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 export const setupSwagger = (app: INestApplication) => {
   const config = new DocumentBuilder()
     .setTitle('PhantoHealth')
     .setDescription('PANTOhealth Backend Developer Technical Assessment')
     .setVersion('1.0')
-    // .addBearerAuth()
     .build();
 
   const documentFactory = () => SwaggerModule.createDocument(app, config);
