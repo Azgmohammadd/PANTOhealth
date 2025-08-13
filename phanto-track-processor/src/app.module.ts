95825f17b28144cd587c3bbd6295393ab3d5837e
azgmohammadd <azgmohammadd@gmail.com>
2025-08-13 21:54:40 +0330

    :sparkles: feat: add generator module

--- phanto-track-processor/src/app.module.ts
+++ phanto-track-processor/src/app.module.ts
@@ -1,17 +1,16 @@
-import { Module } from '@nestjs/common';
+import { Module, OnApplicationBootstrap } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { SignalGeneratorModule } from './modules/signal-generator/signal-generator.module';
 import { SignalProcessorModule } from './modules/signal-processor/signal-processor.module';
 import { LoggerModule } from './modules/logger/logger.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { SignalGeneratorService } from './modules/signal-generator/signal-generator.service';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      isGlobal: true,
+      // isGlobal: true,
       envFilePath: '.env.example',
-      cache: true,
-      expandVariables: true,
     }),
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
@@ -20,9 +19,15 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       }),
       inject: [ConfigService],
     }),
-    SignalGeneratorModule,
     SignalProcessorModule,
+    SignalGeneratorModule,
     LoggerModule,
   ],
 })
-export class AppModule {}
+export class AppModule implements OnApplicationBootstrap {
+  constructor(private readonly signalGenerator: SignalGeneratorService) {}
+
+  async onApplicationBootstrap() {
+    await this.signalGenerator.sendSignals();
+  }
+}
--- phanto-track-processor/src/modules/signal-generator/signal-generator.module.ts
+++ phanto-track-processor/src/modules/signal-generator/signal-generator.module.ts
@@ -1,4 +1,28 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { SignalGeneratorService } from './signal-generator.service';
 
-@Module({})
+const SIGNAL_GENERATOR_RMQ_CLIENT = 'SIGNAL_GENERATOR_RMQ_CLIENT';
+
+@Module({
+  imports: [
+    ClientsModule.registerAsync([
+      {
+        name: SIGNAL_GENERATOR_RMQ_CLIENT,
+        imports: [ConfigModule],
+        useFactory: (configService: ConfigService) => ({
+          transport: Transport.RMQ,
+          options: {
+            urls: [configService.getOrThrow<string>('RMQ_URL')],
+            queue: configService.getOrThrow<string>('RMQ_QUEUE'),
+          },
+        }),
+        inject: [ConfigService],
+      },
+    ]),
+  ],
+  providers: [SignalGeneratorService],
+  exports: [SignalGeneratorService],
+})
 export class SignalGeneratorModule {}
